618859d279682b2fe319fc53b65cba58b83ece4d
emmanuel-194 <konanemmanuel102@gmail.com>
2026-09-01 14:59:32 +0000

    Ajout des differentes modifications

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale // Importation ajoutée ici
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+                    GreetingImage(stringResource(R.string.happy_birthday_text), stringResource(R.string.signature_text))
                 }
             }
         }
@@ -71,7 +72,7 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
     Box {
         Image(
             painter = image,
-            contentDescription = null, // Virgule ajoutée ici
+            contentDescription = null,
             contentScale = ContentScale.Crop,
             alpha = 0.5f
         )
@@ -93,7 +94,7 @@ fun BirthdayCardPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            GreetingImage(message = "Happy Birthday Sam!", from = "From Emma")
+            GreetingImage(stringResource(R.string.happy_birthday_text), stringResource(R.string.signature_text))
         }
     }
 }
